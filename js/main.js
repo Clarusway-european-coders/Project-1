e01932372685f58c2d650344b86b726609ec22b0
Sinan <sinan.sarykaya@gmail.com>
2022-09-04 19:27:04 +0200

    Copyright added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
 const close = document.querySelector(".close");
 const bgFilter = document.querySelector(".background-filter");
+const copyright = document.querySelector(".copyright");
+const codersBox = document.querySelector("#codersBox");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
@@ -17,10 +19,17 @@ document.addEventListener("keyup", (e) => {
   if (e.key == "Escape") {
     navbar.classList.remove("active");
     bgFilter.classList.remove("active");
+    codersBox.classList.remove("active");
   }
 });
 
 bgFilter.addEventListener("click", () => {
   navbar.classList.remove("active");
   bgFilter.classList.remove("active");
+  codersBox.classList.remove("active");
+});
+
+copyright.addEventListener("click", () => {
+  bgFilter.classList.add("active");
+  codersBox.classList.add("active");
 });
